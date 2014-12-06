cc13275b07096189eabf053368c8610f0fcd10b4
chriswalpen <c.walpen@gmx.net>
2014-12-06 22:10:44 +0100

    Bootstrapper to initialize Scribe

---
+++ src/Scribe/ScribeBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Scribe
+{
+    public class ScribeBootstrapper
+    {
+        public void Initialize()
+        {
+            // creating an instance automaticaly initializes Scribe
+            var manager = new LogManager();
+            manager.Initialize();
+        }
+    }
+}
