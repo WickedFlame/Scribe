867a9cd331b402b3b3df419c7648c172496636cc
chriswalpen <c.walpen@gmx.net>
2015-11-06 22:24:01 +0100

    Update

--- src/Tests/Scribe.Test/Component/TraceLogListnerTests.cs
+++ src/Tests/Scribe.Test/Component/TraceLogListnerTests.cs
@@ -138,7 +138,7 @@ namespace Scribe.Test.Component
             //Assert.IsTrue(logprocessor.LogEntries.Count() == 1);
 
             Assert.IsNotNull(entry);
-            Assert.IsTrue(entry.Message.StartsWith("Error message"), entry.Message);
+            Assert.IsTrue(entry.Message.StartsWith("Error message"), entry.ToString());
             //Assert.IsTrue(entry.Message.Contains("StackTrace"));
             //Assert.IsTrue(entry.LogLevel.Equals(LogLevel.Error));
         }
