81d10443ff00aab880426c4b864072733204e6fd
Christian Walpen <c.walpen@gmx.net>
2016-03-31 22:30:09 +0200

    Added Extension

---
+++ src/Scribe/LoggerConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribe
+{
+    public static class LoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration AddTraceWriter(this LoggerConfiguration configuration)
+        {
+            configuration.AddWriter(new TraceLogWriter());
+
+            return configuration;
+        }
+    }
+}
