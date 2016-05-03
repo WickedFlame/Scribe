5a7e62e2c7238c76cbbe3d9d0faeddde06f5f9a3
Christian Walpen <c.walpen@gmx.net>
2016-05-03 23:02:44 +0200

    Compiled nuspec in wrong directory

--- pack.ps1
+++ pack.ps1
@@ -4,13 +4,13 @@ Write-Host $root
 Write-Host "$root\src\Scribe\bin\Release\Scribe.dll"
 
 $version = [System.Reflection.Assembly]::LoadFile("$root\src\Scribe\bin\Release\Scribe.dll").GetName().Version
-$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 $content = (Get-Content $root\src\Scribe.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\build\Scribe.compiled.nuspec
+$content | Out-File $root\src\Scribe.compiled.nuspec
 
-& $root\build\NuGet.exe pack $root\build\Scribe.compiled.nuspec
+& $root\build\NuGet.exe pack $root\src\Scribe.compiled.nuspec
