564bc0c7cde4757565ed00e96f66d645023453d3
Christian Walpen <c.walpen@gmx.net>
2016-05-03 22:38:15 +0200

    Updated nuget build

---
+++ pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\Scribe\bin\Release\Scribe.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\src\Scribe.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\build\Scribe.compiled.nuspec
+
+& $root\build\NuGet.exe pack $root\build\Scribe.compiled.nuspec
